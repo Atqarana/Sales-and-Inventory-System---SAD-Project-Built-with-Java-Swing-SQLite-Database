1527bafef07311202a96daab271331a6fb689be7
Alger Makiputin <algerzxc@gmail.com>
2017-03-02 15:35:12 +0800

    Add files via upload

---
+++ DBConnect.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salesandinventorymanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author alger
+ */
+public class DBConnect {
+    Connection conn = null;
+    public static Connection DBConnect(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:salesandinventory");
+            return conn;
+        }catch(Exception ex){
+            System.out.println("Error" + ex);
+            return null;
+        }
+        
+    }
+    
+}
